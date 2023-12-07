b39fd505604c0c6761d000ef8f74a28fa66d0510
aasma <ubuntu-clone@example.com>
2023-12-07 09:57:04 +0530

    header and footer

--- app/page.js
+++ app/page.js
@@ -2,15 +2,17 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './globals.css';
-//import Header from './header.js';  
+import Header from './header.js';  
 import Scroll from './Scroll';
-//import Footer from './footer';  
+import Footer from './footer';  
 //import Main from './Main';  
 
 export default function Home() {
   return (
     <>
+      <Header/>
       <Scroll/>
+      <Footer/>
     </>
 
   );
