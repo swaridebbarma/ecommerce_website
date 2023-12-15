5865c8a4d22b89d9d38c1807d35c69c08a69da7e
swaridebbarma <swaridebbarma47@gmail.com>
2023-12-15 15:43:32 +0530

    page update

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 "use state"
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './globals.css';
 //import Header from './header.js';  
 import Scroll from './Scroll';
 //import Footer from './footer';  
